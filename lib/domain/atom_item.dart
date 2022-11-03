400564d6be183b0306b92c29367df5f2bebde556
And96 <42185838+And96@users.noreply.github.com>
2022-11-03 20:23:41 +0100

    Update atom_item.dart
    Remove secondary fields

--- lib/domain/atom_item.dart
+++ lib/domain/atom_item.dart
@@ -1,8 +1,8 @@
-import 'package:webfeed/domain/atom_category.dart';
+/*import 'package:webfeed/domain/atom_category.dart';
 import 'package:webfeed/domain/atom_link.dart';
 import 'package:webfeed/domain/atom_person.dart';
 import 'package:webfeed/domain/atom_source.dart';
-import 'package:webfeed/domain/media/media.dart';
+import 'package:webfeed/domain/media/media.dart';*/
 import 'package:webfeed/util/datetime.dart';
 import 'package:webfeed/util/iterable.dart';
 import 'package:xml/xml.dart';
@@ -12,31 +12,31 @@ class AtomItem {
   final String? title;
   final DateTime? updated;
 
-  final List<AtomPerson>? authors;
-  final List<AtomLink>? links;
-  final List<AtomCategory>? categories;
+  //final List<AtomPerson>? authors;
+  //final List<AtomLink>? links;
+  /*final List<AtomCategory>? categories;
   final List<AtomPerson>? contributors;
-  final AtomSource? source;
+  final AtomSource? source;*/
   final String? published;
   final String? content;
   final String? summary;
   final String? rights;
-  final Media? media;
+  final Media? media;*/
 
   AtomItem({
     this.id,
     this.title,
     this.updated,
-    this.authors,
+    /*this.authors,
     this.links,
     this.categories,
     this.contributors,
-    this.source,
+    this.source,*/
     this.published,
     this.content,
-    this.summary,
+    /*this.summary,
     this.rights,
-    this.media,
+    this.media,*/
   });
 
   factory AtomItem.parse(XmlElement element) {
@@ -44,7 +44,7 @@ class AtomItem {
       id: element.findElements('id').firstOrNull?.text,
       title: element.findElements('title').firstOrNull?.text,
       updated: parseDateTime(element.findElements('updated').firstOrNull?.text),
-      authors: element
+      /*authors: element
           .findElements('author')
           .map((e) => AtomPerson.parse(e))
           .toList(),
@@ -61,12 +61,12 @@ class AtomItem {
       source: element
           .findElements('source')
           .map((e) => AtomSource.parse(e))
-          .firstOrNull,
+          .firstOrNull,*/
       published: element.findElements('published').firstOrNull?.text,
       content: element.findElements('content').firstOrNull?.text,
-      summary: element.findElements('summary').firstOrNull?.text,
+      /*summary: element.findElements('summary').firstOrNull?.text,
       rights: element.findElements('rights').firstOrNull?.text,
-      media: Media.parse(element),
+      media: Media.parse(element),*/
     );
   }
 }
